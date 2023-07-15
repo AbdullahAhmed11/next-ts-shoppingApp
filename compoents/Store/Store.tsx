8a97e33f089753ab080c4fd9fcabf3861883583f
Abdullahahmed11 <abdullah77zzx@gmail.com>
2023-07-15 14:47:45 +0300

    first commit

---
+++ compoents/Store/Store.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+
+function Store() {
+    return (
+        <>
+            <Box>
+                
+            </Box>
+        </>
+    )
+}
+export default Store;
